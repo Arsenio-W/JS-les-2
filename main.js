b88f9fd12ff79e5321ae5afde74158fbc23dc867
Arsenio-W <143513029+Arsenio-W@users.noreply.github.com>
2023-09-01 09:48:25 +0200

    Update main.js

---
+++ main.js
@@ -0,0 +1,65 @@
+let a = 18;
+let b = 6;
+
+let i = 15;
+let j = 17;
+let e = 17;
+let f = 4;
+let c = 12;
+let d = 16;
+let k = 29;
+let l = 31;
+console.log('result: ', a + b);
+console.log('result: ', c + d);
+console.log('result: ', c + j );
+console.log('result: ', i + j);
+console.log('result: ', k + l);
+
+
+let m = 15;
+let n = 3;
+let o = 10
+let p = 38
+let q = 6
+let r = 8
+//ik weet het is veel variables
+console.log('result: ', m * n);
+console.log('result: ', o * p);
+console.log('result: ', q * r);
+console.log('result: ', m * r);
+console.log('result: ', q * o);
+
+salaris = 2000;
+salarisverhoging = 0.05;
+
+verhoging = salaris * salarisverhoging
+
+console.log('verhoging:', verhoging)
+
+let s = 4;
+let t = 2;
+console.log('result: ', s / t);
+console.log('result: ', q / t);
+console.log('result: ', p / s);
+console.log('result: ', k / n);
+console.log('result: ', a / r);
+
+let x = 4;
+let y = 0;
+console.log('result: ',  x / y);
+
+let huisprijs = 4000000;
+let rente = 0.025;
+totaleprijs = huisprijs * rente
+console.log('totale huisprijs: ', totaleprijs);
+
+jasprijs = 100
+btw = 1.21
+helejasprijs = jasprijs * btw
+console.log('jasprijs plus btw: ', helejasprijs);
+
+type string = "Arsenio"
+//ik begrijp nog steeds niet hoe je een string gebruikt
+helenaam = voornaam + achternaam
+console.log('Mijn hele naam is: ',helenaam);
+
